423b958003adeb7c4bb255a57598d2c7a2da7037
Samson <samsoned@hotmail.com>
2022-07-27 16:34:32 +0200

    reorganize code and add user class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule
