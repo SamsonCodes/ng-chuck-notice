0b199b087708718796fb42de478ff13806ab9081
Samson <samsoned@hotmail.com>
2022-09-09 10:15:16 +0200

    Add live announcer to example table

--- src/app/components/example-table/example-table.component.ts
+++ src/app/components/example-table/example-table.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort, Sort} from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 export interface PeriodicElement {
   name: string;
@@ -37,7 +38,7 @@ export class ExampleTableComponent implements AfterViewInit {
 
   filterInput: string = "";
   
-  constructor() { }
+  constructor(private _liveAnnouncer: LiveAnnouncer) { }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -49,5 +50,18 @@ export class ExampleTableComponent implements AfterViewInit {
     let filterValue = this.filterInput.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
