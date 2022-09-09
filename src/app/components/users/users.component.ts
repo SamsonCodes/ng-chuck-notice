a1213cd02e50724e7c0bb633ea3766732a10bd11
Samson <samsoned@hotmail.com>
2022-09-09 10:35:45 +0200

    Change user table into material table including pagination, sorting, filter, live announcer

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 import { UserService } from '../../services/user.service';
 import { User } from '../../classes/user';
@@ -10,7 +14,7 @@ import { User } from '../../classes/user';
   styleUrls: ['./users.component.css'],
   providers: [UserService]
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit {
   users: User[] = [];
 
   newUser = {
@@ -27,14 +31,28 @@ export class UsersComponent implements OnInit {
     password: '1234'
   };
   userForm = this.formBuilder.group(this.defaultFormValues);  
+
+  @ViewChild(MatPaginator) paginator: any = MatPaginator;
+  @ViewChild(MatSort) sort: any = MatSort;
+
+  displayedColumns: string[] = ['name', 'userGroup', 'penalties', 'actions'];
+  dataSource = new MatTableDataSource<User>([]);
+
+  filterInput: string = "";
   
   constructor(
     private userService: UserService, 
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private _liveAnnouncer: LiveAnnouncer
   ) {}
 
   ngOnInit(): void {
     this.refreshUserList();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   onSubmit(){    
@@ -53,11 +71,32 @@ export class UsersComponent implements OnInit {
   refreshUserList(){
     this.userService.getUsers().subscribe((res) => {
       this.users = res as User[];
+      this.dataSource.data = this.users;
     });
   }
 
   resetForm(){
     this.userForm.setValue(this.defaultFormValues);
     this.userForm.reset(this.userForm.value);
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  onFilterChange() {
+    console.log(this.filterInput);
+    let filterValue = this.filterInput.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
   }
 }
